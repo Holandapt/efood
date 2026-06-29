c2fe1860ecf4113c8c79b9a802d2a1f11f4a5d85
lucas <holanda.ptr@gmail.com>
2026-06-29 20:08:55 +0100

    Finalizando pages

--- src/pages/Perfil/index.tsx
+++ src/pages/Perfil/index.tsx
@@ -12,6 +12,20 @@ const Product: Products[] = [
     description:
       'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
     image: pizza
+  },
+  {
+    id: 2,
+    title: 'Pizza Marguerita',
+    description:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+    image: pizza
+  },
+  {
+    id: 3,
+    title: 'Pizza Marguerita',
+    description:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+    image: pizza
   }
 ]
 
